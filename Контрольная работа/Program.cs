fab3ffb056b418e1942639be406ba86987828b4d
Katerina <m-a-c-u-k-a@mail.ru>
2023-01-13 02:41:22 +0700

    Контрольная работа

--- Контрольная работа/Program.cs
+++ Контрольная работа/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+string[] newarray = new string[array.Length];
+
+
+FillArray(array);
+PrintArray(array);
+
+NewArray(array, newarray);
+PrintArray(newarray);
+
+void NewArray(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
+
+void FillArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент: ");
+        array [i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
